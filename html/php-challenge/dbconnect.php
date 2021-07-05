04fd2547b8f94954d43fac09898107e1bf96ae86
小川　木楠 <>
2021-07-05 20:30:37 +0900

    提出

--- html/php-challenge/dbconnect.php
+++
@@ -1,6 +0,0 @@
-<?php
-try {
-    $db = new PDO('mysql:dbname=test;host=mysql;charset=utf8', 'root', 'root');
-} catch (PDOException $e) {
-    echo 'DB接続エラー： ' . $e->getMessage();
-}
--- html/php-pre-challenge3/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$limit = $_GET['target'];
-
-$dsn = 'mysql:dbname=test;host=mysql';
-$dbuser = 'test';
-$dbpassword = 'test';
